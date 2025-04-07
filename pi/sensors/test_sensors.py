e7f8507054d7d47fdcb7709c6f2a888a82b28ef9
Isaac George <git-mail@isaacgeorge.net>
2025-04-07 12:04:49 -0500

    Update

--- pi/sensors/test_sensors.py
+++ pi/sensors/test_sensors.py
@@ -5,7 +5,7 @@ import audioop
 
 p = pyaudio.PyAudio()
 WIDTH = 2
-CHANNELS = 2
+CHANNELS = 1
 RATE = int(p.get_default_input_device_info()['defaultSampleRate'])
 DEVICE = p.get_default_input_device_info()['index']
 
